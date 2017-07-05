56605fcb3cd258856e0d984a67401ae6adbfb091
valentinCo <eersel-val@hotmail.com>
2017-07-05 13:36:10 +0200

    Exercice 8 fini

--- array/app.js
+++ array/app.js
@@ -1,12 +1,14 @@
 var mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
-
-	console.log (mois [2]);
-	console.log (mois [5]);
-	console.log (mois [10]);
-	console.log (mois [7] = "août");
+var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+	console.log(mois [2]);
+	console.log(mois [5]);
+	console.log(mois [10]);
+	console.log(mois [7] = "août");
 
 	for(var i= 0; i < mois.length; i++) {
 		console.log (mois[i]);
 	}
 
+	array.push("Courgette");
+	console.log(array);
 
