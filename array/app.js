fd9be6f8c17f96bb9cbf6b26e64ab68de5be4aa9
valentinCo <eersel-val@hotmail.com>
2017-07-05 11:51:38 +0200

    Exercice 4

--- array/app.js
+++ array/app.js
@@ -1,5 +1,6 @@
-var mois = ["janvier", "février", "mars", "avril", "mai", "juin" "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
+var mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
 
 	console.log (mois [2]);
 	console.log (mois [5]);
 	console.log (mois [10]);
+	
